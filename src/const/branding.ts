0fb62cc5aeff183f04afec472107409e76e53c00
Ryder.Tsui <108246720+CRD-1225@users.noreply.github.com>
2025-06-23 09:15:39 +0800

    Update branding.ts

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = 'TsuiChat Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
+export const BRANDING_NAME = 'TsuiChat';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'Hub';
 
 export const BRANDING_URL = {
   help: undefined,
@@ -17,13 +17,13 @@ export const BRANDING_URL = {
 
 export const SOCIAL_URL = {
   discord: 'https://discord.gg/AYFPHvv2jT',
-  github: 'https://github.com/lobehub',
-  medium: 'https://medium.com/@lobehub',
-  x: 'https://x.com/lobehub',
-  youtube: 'https://www.youtube.com/@lobehub',
+  github: 'https://github.com/hub',
+  medium: 'https://medium.com/@hub',
+  x: 'https://x.com/hub',
+  youtube: 'https://www.youtube.com/@hub',
 };
 
 export const BRANDING_EMAIL = {
-  business: 'hello@lobehub.com',
-  support: 'support@lobehub.com',
+  business: 'hello@hub.com',
+  support: 'support@hub.com',
 };
